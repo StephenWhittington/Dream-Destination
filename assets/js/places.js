52edddffb78e5e942dc0055dd11d83900a2a7fe0
Ubuntu <ubuntu@ip-172-31-82-244.ec2.internal>
2019-06-10 22:51:45 +0000

    Google map added with lat/lng with places api inside initMap function

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,14 +1,23 @@
-function navSearch() {
-  var input = document.getElementById('navBarSearch');
-  new google.maps.places.Autocomplete(input);
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    zoom: 3,
+    center: new google.maps.LatLng(46.619261,-33.134766)
+  });
+
+  function navSearch() {
+    var input = document.getElementById('navBarSearch');
+    new google.maps.places.Autocomplete(input);
+  }
+
+  google.maps.event.addDomListener(window, 'load', navSearch);
+
+  function buttonSearch() {
+    var input = document.getElementById('whereTo');
+    new google.maps.places.Autocomplete(input);
+  }
+
+  google.maps.event.addDomListener(window, 'load', buttonSearch);
+
+
 }
-
-google.maps.event.addDomListener(window, 'load', navSearch);
-
-function buttonSearch() {
-  var input = document.getElementById('whereTo');
-  new google.maps.places.Autocomplete(input);
-}
-
-google.maps.event.addDomListener(window, 'load', buttonSearch);
-
