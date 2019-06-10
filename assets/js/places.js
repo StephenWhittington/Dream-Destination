1bd67f620b09325eb4278ea8bc3fecf26e6d7508
Ubuntu <ubuntu@ip-172-31-82-244.ec2.internal>
2019-06-10 20:03:39 +0000

    Updated google places autocomplete in modal, with bug fix

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,6 +1,14 @@
-function initialize() {
+function navSearch() {
   var input = document.getElementById('navBarSearch');
   new google.maps.places.Autocomplete(input);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', navSearch);
+
+function buttonSearch() {
+  var input = document.getElementById('whereTo');
+  new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', buttonSearch);
+
