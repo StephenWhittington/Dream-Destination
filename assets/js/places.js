7596f3f26e0a065a1246b0bd5c06599e23aa0c85
Ubuntu <ubuntu@ip-172-31-82-244.ec2.internal>
2019-06-20 22:29:11 +0000

    updated nav search bar with autocomplete api

--- assets/js/places.js
+++ assets/js/places.js
@@ -5,6 +5,7 @@ function initMap() {
     zoom: 3,
     center: new google.maps.LatLng(46.619261, -33.134766)
   });
+
   var input = document.getElementById('searchMapInput');
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
@@ -40,14 +41,43 @@ function initMap() {
     marker.setVisible(true);
   });
 
+  var navInput = document.getElementById('navBarSearch');
 
-  // Autocomplete in navbar search bar for users to enter a location
-  function navSearch() {
-    var input = document.getElementById('navBarSearch');
-    new google.maps.places.Autocomplete(input);
-  }
+  var autocomplete = new google.maps.places.Autocomplete(navInput);
+  autocomplete.bindTo('bounds', map);
 
-  google.maps.event.addDomListener(window, 'load', navSearch);
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+  
+  document.getElementById('navBtn').onclick = function() {
+    autocomplete;
+  };
+  
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    }
+    else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setIcon(({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+  }); 
 
   // Button autocomplete in modal also for users who want to enter a location
   function buttonSearch() {
