ecce39aa471d57d26726f0f6e2bfc83b67e6e18b
Ubuntu <ubuntu@ip-172-31-82-244.ec2.internal>
2019-06-24 19:43:19 +0000

    change to navbar just site logo, removed search bar and button as not needed

--- assets/js/places.js
+++ assets/js/places.js
@@ -41,44 +41,6 @@ function initMap() {
     marker.setVisible(true);
   });
 
-  var navInput = document.getElementById('navBarSearch');
-
-  var autocomplete = new google.maps.places.Autocomplete(navInput);
-  autocomplete.bindTo('bounds', map);
-
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
-  
-  document.getElementById('navBtn').onclick = function() {
-    autocomplete;
-  };
-  
-  autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
-    var place = autocomplete.getPlace();
-
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    }
-    else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-    marker.setIcon(({
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-  }); 
-
   // Button autocomplete in modal also for users who want to enter a location
   function buttonSearch() {
     var input = document.getElementById('whereTo');
@@ -86,4 +48,5 @@ function initMap() {
   }
 
   google.maps.event.addDomListener(window, 'load', buttonSearch);
+
 }
